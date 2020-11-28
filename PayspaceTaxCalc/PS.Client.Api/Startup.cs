18534d7b7d72bd0171cc1d50c24af13c1e1f7b09
Johan Potgieter <Johan.Potgieter@parmalat.co.za>
2020-11-28 18:01:55 +0200

    Added TaxRate and TaxType

--- PayspaceTaxCalc/PS.Client.Api/Startup.cs
+++ PayspaceTaxCalc/PS.Client.Api/Startup.cs
@@ -21,7 +21,6 @@ namespace PS.Client.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureCors();
@@ -31,7 +30,6 @@ namespace PS.Client.Api
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
