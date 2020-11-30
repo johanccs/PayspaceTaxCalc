d39a3bfa44de80b197f6950a7ec2f7b941238bf8
Johan Potgieter <johan.potgieter@muchasphalt.com>
2020-11-30 07:15:30 +0200

    Razor pages added. Gettings values from view

---
+++ PayspaceTaxCalc/PS.Client.Web/Controllers/TaxController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PS.Client.Web.LocalService;
+using PS.Client.Web.Models;
+using PS.Contracts.Services;
+
+namespace PS.Client.Web.Controllers
+{
+    public class TaxController : Controller
+    {
+        #region Readonly Fields
+
+        private readonly IConfiguration _config;
+      
+        #endregion
+
+        #region Constructor
+
+        public TaxController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        #endregion
+
+        public IActionResult Index()
+        {
+            var taxCalc = new TaxCalculationModel();
+
+            return View(taxCalc);
+        }
+
+        public IActionResult Create()
+        {
+            var taxCalc = new TaxCalculationModel();
+
+            return View(taxCalc);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TaxCalculationModel tc)
+        {
+            if (!ModelState.IsValid)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+
+            var result = ApiHandler.Handle(tc, _config);
+
+            ViewData["Result"] = result;
+
+            return RedirectToAction("Success");
+        }
+    }
+}
---
+++ PayspaceTaxCalc/PS.Client.Web/LocalService/ApiHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using PS.Client.Web.Models;
+
+namespace PS.Client.Web.LocalService
+{
+    public static class ApiHandler
+    {
+        public static string Handle(TaxCalculationModel tc, IConfiguration config)
+        {
+            var baseUrl = config["BaseApiUrl"];
+
+            return null;
+        }
+    }
+}
